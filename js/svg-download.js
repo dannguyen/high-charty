5c0bfd562b7e101fd35dfdc392b6ea99cd98a75a
Sean Connolly <sean.connolly@gmail.com>
2014-06-30 18:02:51 -0400

    PNG download works

--- js/svg-download.js
+++ js/svg-download.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
 
 	$('#download-image-link a.prep').click(function(e) {
+		var canvas = document.getElementById('canvas');
 
 		$('#download-image-link a.prep').hide();
 	    e.preventDefault();
@@ -11,6 +12,10 @@ $(document).ready(function() {
 		var element = $('.highcharts-container');
 	    source.push(element.html());
 
+		var canvasContext = canvas.getContext("2d");
+		// var svg = $.trim(document.getElementById("chartContainer").innerHTML);
+		canvasContext.drawSvg(element.html(),0,0);
+
 	    var url = window.URL.createObjectURL(new Blob(source, { "type" : "text\/xml" }));
 
 	    $('#download-image-link a.download-svg').attr({
@@ -18,7 +23,7 @@ $(document).ready(function() {
 	    	download: filename + '.svg'
 		}).show();
 		$('#download-image-link a.download-png').attr({
-			// href: source.toDataURL('png'), use canvg for this
+			href: canvas.toDataURL('png'),
 			download: filename + '.png'
 		}).show();
 

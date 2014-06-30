794ea14147ac692561d18f7b987a892df087e9c7
Sean Connolly <sean.connolly@gmail.com>
2014-06-30 18:07:22 -0400

    Add comments to svg-download

--- js/svg-download.js
+++ js/svg-download.js
@@ -9,13 +9,18 @@ $(document).ready(function() {
 	    var filename = 'untitled'; // TODO: set up useful filename structure based on chart contents, date
 		var source = new Array();
 
+		// Grab the chart from this container, put in array
 		var element = $('.highcharts-container');
 	    source.push(element.html());
+
+	    // Prep canvas and draw SVG in it
 		var canvasContext = canvas.getContext("2d");
 		canvasContext.drawSvg(element.html(),0,0);
 
+		// Create new blob with SVG in it
 	    var url = window.URL.createObjectURL(new Blob(source, { "type" : "text\/xml" }));
 
+	    // Set up download links for blob and PNG from canvas; probably not supported cross-browser
 	    $('#download-image-link a.download-svg').attr({
 	    	href: url,
 	    	download: filename + '.svg'

c216802a911f59c3609bf52c97a8bf9e8f7a1c57
Dan Nguyen <dansonguyen@gmail.com>
2014-07-01 17:04:15 -0400

    parsing test done

--- app/scripts/lib/Charty.DataParser.js
+++ app/scripts/lib/Charty.DataParser.js
@@ -17,6 +17,75 @@
     );
   }
 
+  // {arr} is an Array of Arrays in which the first row
+  // contains the headers/attributes
+  //
+  //   arr = [['x', 'y'], [0, 0], [2, 5]]
+  //
+  // return: An Array of Hashes
+  //  [{x: 0, y: 0}, {x: 2, y: 5}]
+
+  DataParser.prototype.arraysToFlatDataSet = function(arr){
+    var headers = arr[0];
+
+    return _.map( arr.slice(1), function(row){
+      return _.object(headers, row);
+    });
+  }
+
+  // {arr} is assumed to be a FlatDataSet-like array:
+  //
+  //    arr = [{a: 0, b: 0, c: 999}, {a: 2, b: 5}]
+  //
+  // {hmap} is a Hash of key-value pairs, in which the key
+  //   is the :key to keep, and the :value is the new alias
+  //   for the current :key. Any keys not in arr's objects will
+  //   be dropped
+  //
+  //    hmap = { a: 'x', b: 'y'};
+  //
+  // returns: An Array of Hashes
+  //
+  //    [{x: 0, y: 0}, {x: 2, y: 5}]
+
+  DataParser.prototype.pickAndSwitchKeys = function(arr, hmap){
+    return _.map(arr, function(obj){
+      return _.reduce(hmap, function(memo, oldKey, newKey){
+        memo[newKey] = obj[oldKey];
+
+        return memo
+      }, {});
+    })
+  };
+
+  // given a FlatDataSet {arr} and a string {key}
+  // returns: a Hash in which the keys are the groupBy {key}
+  //  and the values are the array of Hashes with the same {key}
+  DataParser.prototype.groupByKey = function(arr, key){
+    return _.groupBy(arr, key);
+  }
+
+  // given a FlatDataSet as {arr}, and an {dataOpts} Hash like:
+  // {seriesKey: 'genre', x: 'a', y: 'b'}
+  // returns an Array
+  // [{ name: 'Horror', data: [{x: 2, y: 3}] }, {name: 'Romance', data: []}]
+  DataParser.prototype.toHighChartsFormat = function(orgArr, dataOpts){
+    var self = this;
+    var keyMap = _.omit(dataOpts, 'seriesKey')
+    var groupedArr = self.groupByKey(orgArr, dataOpts.seriesKey);
+
+    // ugh inefficient to loop again
+    return _.reduce(groupedArr, function(memoArr, dArr, seriesName){
+      var seriesObj = { name: seriesName };
+      seriesObj.data = self.pickAndSwitchKeys(dArr, keyMap);
+      memoArr.push(seriesObj);
+
+      return memoArr
+    }, []);
+
+  }
+
+
 
   return DataParser;
 })();
--- test/spec/test.js
+++ test/spec/test.js
@@ -2,9 +2,6 @@
 
 (function () {
     'use strict';
-
-
-
         describe('Charty.DataParser', function(){
           it('should exist', function(){
             assert.equal(_.isUndefined(Charty.DataParser), false);
@@ -28,6 +25,8 @@
               var r = parser.parseRawCSV("a,b,c");
               assert.equal(r[0].length, 3);
               assert.equal(r[0][2], 'c');
+
+              assert.deepEqual(r, [['a', 'b', 'c']])
             });
           });
 
@@ -35,19 +34,83 @@
           describe('Charty.DataParser.transposeArray', function(){
             it('turns rows into columns', function(){
               var arr = [[1,2,3], [4,5,6]];
-              _.isEqual(parser.transposeArray(arr), [[1,4],[2,5],[3,6]])
-                .should.equal(true);
+              parser.transposeArray(arr).should.deep.equal( [[1,4],[2,5],[3,6]] );
             });
 
             it('turns cols into rows', function(){
               var arr = [[1,4],[2,5],[3,6]];
-              _.isEqual(parser.transposeArray(arr), [[1,2,3], [4,5,6]])
-                .should.equal(true);
+              parser.transposeArray(arr).should.deep.equal( [[1,2,3], [4,5,6]]);
             });
           });
 
 
+          describe('Chart.DataParser.arraysToFlatDataSet', function(){
+            var arr = [['x', 'y', 'series', 'category'],
+                [10, 100, 'Apples', 'Winter'],
+                [22, 200, 'Oranges', 'Winter']];
 
+            it('maps values to first row of array', function(){
+              var h = parser.arraysToFlatDataSet(arr)[1];
+
+              h.should.deep.equal(
+                {x: 22, y: 200, series: 'Oranges', category: 'Winter'});
+            });
+          });
+
+
+          describe('Chart.DataParser.pickAndSwitchKeys', function(){
+            var arr = [{a: 42, b: 60, c: 100}, {a: 7, b: 10, c: 99}]
+            var newarr = parser.pickAndSwitchKeys(arr, {x: 'a', y: 'b'});
+
+            it('renames and filters the keys', function(){
+              newarr.should.deep.equal([{x: 42, y: 60}, {x: 7, y: 10 }]);
+            });
+          });
+
+
+          describe('Chart.DataParser.groupByKey', function(){
+            var arr = [
+              { genre: 'fruit', name: 'apple', price: 2.50, quantity: 100 },
+              { genre: 'animal', name: 'cat', price: 62.2, quantity: 42 },
+              { genre: 'fruit', name: 'orange', price: 1.50, quantity: 99 }
+            ];
+
+            it('creates a new Hash of grouped by key objects', function(){
+              var hsh = parser.groupByKey(arr, 'genre');
+
+              hsh.should.deep.equal({
+                  'fruit': [
+                    { genre: 'fruit', name: 'apple', price: 2.50, quantity: 100 },
+                    { genre: 'fruit', name: 'orange', price: 1.50, quantity: 99 }
+                  ], 'animal': [
+                    { genre: 'animal', name: 'cat', price: 62.2, quantity: 42 }
+                  ]
+                });
+            });
+          });
+
+
+          describe('Chart.DataParser.toHighChartsFormat', function(){
+            var arr = [
+              { country: 'US', year: 1980, gdp: 500 },
+              { country: 'US', year: 1990, gdp: 600 },
+              { country: 'JP', year: 1980, gdp: 700 },
+              { country: 'JP', year: 1990, gdp: 400 }
+            ];
+
+            it('conveniently returns a Highcharts-friendly object', function(){
+              var seriesarr = parser.toHighChartsFormat( arr,
+                { x: 'year', y: 'gdp', seriesKey: 'country'}
+              )
+
+              seriesarr.should.deep.equal(
+                [
+                  { name: 'US', data: [{x: 1980, y: 500}, {x: 1990, y: 600}] },
+                  { name: 'JP', data: [{x: 1980, y: 700}, {x: 1990, y: 400}] },
+                ]
+              );
+            });
+          })
         });
 
 })();

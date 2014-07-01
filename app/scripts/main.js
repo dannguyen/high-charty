0720600bdf83064c65144177aee688b43520545d
Dan Nguyen <dansonguyen@gmail.com>
2014-07-01 11:50:31 -0400

    added a router

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('\'Allo \'Allo!');
+// Sample router
+// from: https://github.com/flatiron/director
+
+var author = function () { console.log("author"); };
+      var books = function () { console.log("books"); };
+      var viewBook = function (bookId) {
+        console.log("viewBook: bookId is populated: " + bookId);
+      };
+
+      var routes = {
+        '/author': author,
+        '/author/:lastName/:firstName': function(lastName, firstName){
+          console.log("This is the author page for: " + firstName + " " + lastName);
+        },
+        '/books': [books, function() {
+          console.log("An inline route handler.");
+        }],
+        '/books/view/:bookId': viewBook
+      };
+
+      var router = Router(routes);
+
+      router.init();
